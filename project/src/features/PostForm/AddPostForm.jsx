ce3b51956248acae3860c720fce544e7412f7b5b
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-07-15 13:41:35 +0900

    feat.axios
    백엔드 통신 연결
    slice 구성
    formdata 생성 및 전송

--- project/src/features/PostForm/AddPostForm.jsx
+++ project/src/features/PostForm/AddPostForm.jsx
@@ -1,107 +1,119 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { __postLists } from '../../redux/modules/listSlice';
+import useInput from '../../hooks/useInput';
 
 function AddPostForm() {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-	const [title, setTitle] = useState("");
-	const [name, setName] = useState("");
-	const [password, setPassword] = useState("");
-	const [content, setContent] = useState("");
+    const [title, onChangeTitle] = useInput('');
+    const [username, onChangeName] = useInput('');
+    const [password, onChangePassword] = useInput('');
+    const [content, onChangeContent] = useInput('');
 
-	const onChangeTitle = (event) => {
-		setTitle(event.target.value);
-		console.log(title);
-	};
+    const onClickCancelButton = () => {
+        navigate(-1);
+    };
 
-	const onChangeName = (event) => {
-		setName(event.target.value);
-		console.log(name);
-	};
+    const fileInput = useRef();
 
-	const onChangePassword = (event) => {
-		setPassword(event.target.value);
-		console.log(password);
-	};
+    const onClickConfirmButton = async (event) => {
+        event.preventDefault();
+        const newList = {
+            title,
+            content,
+            username,
+            password,
+            files: fileInput.current.files,
+        };
+        console.log(newList);
+        await dispatch(__postLists(newList));
+        // navigate('/detail');
+    };
 
-	const onChangeContent = (event) => {
-		setContent(event.target.value);
-		console.log(content);
-	};
-
-	const onClickCancelButton = () => {
-		navigate(-1);
-	};
-
-	const onClickConfirmButton = () => {
-		navigate("/detail");
-	};
-
-	return (
-		<PostContainer>
-			<InputContainer>
-				<TitleInput value={title} onChange={onChangeTitle} />
-				<InputPersonal>
-					<InputName value={name} onChange={onChangeName} />
-					<InputPassword value={password} onChange={onChangePassword} />
-				</InputPersonal>
-				<InputContent value={content} onChange={onChangeContent} />
-			</InputContainer>
-			<PostButtonContainer>
-				<button onClick={onClickCancelButton}>{"취소"}</button>
-				<button onClick={onClickConfirmButton}>{"작성"}</button>
-			</PostButtonContainer>
-		</PostContainer>
-	);
+    return (
+        <PostContainer onSubmit={onClickConfirmButton}>
+            <InputContainer>
+                <TitleInput value={title} onChange={onChangeTitle} />
+                <InputPersonal>
+                    <InputName
+                        autoComplete="current-username"
+                        value={username}
+                        onChange={onChangeName}
+                    />
+                    <InputPassword
+                        type="password"
+                        value={password}
+                        onChange={onChangePassword}
+                        autoComplete="current-password"
+                    />
+                </InputPersonal>
+                <InputContent value={content} onChange={onChangeContent} />
+            </InputContainer>
+            <input
+                ref={fileInput}
+                type="file"
+                id="file-input"
+                name="image"
+                multiple
+            />
+            <PostButtonContainer>
+                <button onClick={onClickCancelButton}>{'취소'}</button>
+                <button onClick={onClickConfirmButton}>{'작성'}</button>
+            </PostButtonContainer>
+        </PostContainer>
+    );
 }
 
 export default AddPostForm;
 
-const PostContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	flex-direction: column;
-	width: 100%;
-	height: 100%;
+const PostContainer = styled.form`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
 `;
 
 const InputContainer = styled.div`
-	display: flex;
-	/* align-items: center; */
-	justify-content: left;
-	flex-direction: column;
-	gap: 20px;
+    display: flex;
+    /* align-items: center; */
+    justify-content: left;
+    flex-direction: column;
+    gap: 20px;
 `;
 
 const TitleInput = styled.input`
-	border: none;
-	border-bottom: 1px solid black;
-	width: 100%;
+    border: none;
+    border-bottom: 1px solid black;
+    width: 100%;
 `;
 
 const InputPersonal = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: flex-start;
-	flex-direction: column;
-	gap: 20px;
-	width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
 `;
 
 const InputName = styled.input`
-	margin-right: auto;
+    margin-right: auto;
 `;
 
 const InputPassword = styled.input`
-	margin-right: auto;
+    margin-right: auto;
 `;
 
 const InputContent = styled.textarea``;
 
 const PostButtonContainer = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	gap: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
 `;
